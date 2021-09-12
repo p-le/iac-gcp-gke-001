ff2ff7841516f5417873bd71fbaea1b6583f8256
Phu Le <phu.le.dev@gmail.com>
2021-09-12 05:18:57 +0000

    FIX get-gke-crendentials

--- src/app/backend.tf
+++ src/app/backend.tf
@@ -8,7 +8,7 @@ terraform {
 data "terraform_remote_state" "cluster" {
   backend = "gcs"
   config = {
-    bucket  = "asia-northeast1-terraform-state"
-    prefix  = "gke-001"
+    bucket = "asia-northeast1-terraform-state"
+    prefix = "gke-001"
   }
 }
